ef7bc76e06da7f04b95b50f46a207890446d6e84
azaremoth <azaremoth@users.noreply.github.com>
2019-12-07 18:47:32 +0100

    initiated

---
+++ modinfo.lua
@@ -0,0 +1,12 @@
+return {
+  name = 'tcampaign',
+  game = 'tcampaign',
+  shortName = 'tcampaign',
+  description = 'Single player campaign content for The Cursed',
+  version = '$VERSION',
+  mutator = 'Official',
+  modtype = 1,
+  depend = {
+      'The Cursed $VERSION',
+  },
+}
