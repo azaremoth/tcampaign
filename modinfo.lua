eb64354f957cdf476706517f446ac30193c35e6c
azaremoth <azaremoth@users.noreply.github.com>
2019-12-25 15:49:45 +0100

    VERSION{0.101}

--- modinfo.lua
+++ modinfo.lua
@@ -2,7 +2,7 @@ return {
   name = 'tcampaign',
   game = 'tcampaign',
   shortName = 'tcampaign',
-  description = 'Single player campaign content for The Cursed',
+  description = 'Single player campaign content for The Cursed (needs tchobby)',
   version = '$VERSION',
   mutator = 'Official',
   modtype = 1,
